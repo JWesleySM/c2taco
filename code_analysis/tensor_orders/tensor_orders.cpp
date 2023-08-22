2c30e07dba57e2c760d41e0373ef559a61510782
JWesleySM <jwesley1442@gmail.com>
2023-08-22 15:47:34 +0100

    Implement array recover analysis

--- code_analysis/tensor_orders/tensor_orders.cpp
+++ code_analysis/tensor_orders/tensor_orders.cpp
@@ -64,6 +64,7 @@ public:
   clang::Rewriter Rewriter_;
   std::map<clang::VarDecl*, clang::BinaryOperator*>Definitions_;
   std::set<clang::VarDecl*> LoopIterators_;
+  std::set<clang::Stmt*> Loops_;
   clang::Expr* ReturnValue_ = nullptr;
 
 
@@ -107,6 +108,7 @@ public:
 
   bool VisitForStmt(clang::ForStmt* FS){
     LoopIterators_.insert(getVariableDeclaration(getVariableReference(FS->getCond())));
+    Loops_.insert(FS);
     return true;
   }
 
@@ -119,6 +121,118 @@ public:
 class TensorOrdersConsumer : public clang::ASTConsumer{
 private:
   TensorOrdersVisitor* Visitor_;
+
+  bool incrementPointer(clang::Stmt* S){
+    if(clang::UnaryOperator* UO = clang::dyn_cast<clang::UnaryOperator>(S)){
+      //llvm::outs() << "Possible pointer move " << Visitor_->Rewriter_.getRewrittenText(UO->getSourceRange()) << "\n";
+      if(UO->isIncrementOp() || UO->isDecrementOp()){
+        return true;
+      }
+    }
+
+    for(auto& Child : S->children())
+      if(incrementPointer(Child))
+        return true;
+
+    return false;
+  }
+
+  void redefinePointer(clang::Stmt* S, clang::VarDecl* Pointer, bool* Redefine){
+    // If we find a definition of the pointer variable in the body of this loop
+    // we do not consider a possible pointer movement because such definition
+    // means ponting to a specific part of the manipulated array, e.g., the
+    // beginning or a next column
+    if(clang::BinaryOperator* BO = clang::dyn_cast<clang::BinaryOperator>(S)){
+      if(BO->isAssignmentOp()){
+        clang::VarDecl* AssignedVar = getVariableDeclaration(getVariableReference(BO->getLHS()));
+        if(AssignedVar == Pointer){
+          if(!incrementPointer(BO)){
+            //llvm::outs() << "Assign to pointer: " << Visitor_->Rewriter_.getRewrittenText(BO->getSourceRange()) << "\n";
+            *Redefine = true;
+            return;
+          }
+        }
+      }
+    }
+
+    if(clang::ForStmt* FS = clang::dyn_cast<clang::ForStmt>(S))
+      return;
+
+
+    for(auto& Child : S->children())
+      redefinePointer(Child, Pointer, Redefine);
+      if(*Redefine)
+        return;
+
+    return;    
+  }
+
+  void movePointer(clang::Stmt* S, clang::VarDecl* Pointer, bool *Move){
+    //llvm::outs() << "'''''''''''''''''' ";
+    //llvm::outs() << Visitor_->Rewriter_.getRewrittenText(S->getSourceRange()) << "\n";
+    if(clang::UnaryOperator* UO = clang::dyn_cast<clang::UnaryOperator>(S)){
+      //llvm::outs() << "Possible pointer move " << Visitor_->Rewriter_.getRewrittenText(UO->getSourceRange()) << "\n";
+      if(UO->isIncrementOp() || UO->isDecrementOp()){      
+        clang::VarDecl* Var = getVariableDeclaration(getVariableReference(UO));
+        //llvm::outs() << "Possible pointer move " << Visitor_->Rewriter_.getRewrittenText(UO->getSourceRange()) << "\n";
+        if(Var == Pointer){
+          //llvm::outs() << "moves pointer " << Var->getNameAsString() << "\n";
+          *Move = true;
+          return;
+        }
+      }
+    }
+    
+    if(clang::ForStmt* FS = clang::dyn_cast<clang::ForStmt>(S))
+      S = FS->getBody();
+
+    for(auto& Child : S->children())
+      movePointer(Child, Pointer, Move);
+      if(*Move)
+        return;
+
+    return;
+  }
+
+  int recoverArray(clang::UnaryOperator* OP, bool Output){
+    //llvm::outs() << "Recovering array: " << Visitor_->Rewriter_.getRewrittenText(OP->getSourceRange()) << "\n";
+    clang::VarDecl* PointerVar = getVariableDeclaration(getVariableReference(OP));
+    std::map<clang::VarDecl*, bool>Coeffs;
+    for(auto& L : Visitor_->Loops_){
+      bool MovePointer = false;
+      bool RedefinePointer = false;
+      if(clang::ForStmt* FS = clang::dyn_cast<clang::ForStmt>(L)){
+        clang::VarDecl* LoopIterator = getVariableDeclaration(getVariableReference(FS->getCond()));
+        movePointer(FS->getBody(), PointerVar, &MovePointer);
+        redefinePointer(FS->getBody(), PointerVar, &RedefinePointer);
+        Coeffs[LoopIterator] = MovePointer;
+        if(!Output){
+          if(RedefinePointer)
+            Coeffs[LoopIterator] = false;
+        }
+      }
+
+      if(clang::WhileStmt* WS = clang::dyn_cast<clang::WhileStmt>(L)){
+        //WS->getBody()
+        continue;
+      }
+      if(clang::DoStmt* DS = clang::dyn_cast<clang::DoStmt>(L)){
+        //DS->getBody()
+        continue;
+      }
+    }
+
+    int Order = 0;
+    for(const auto& [Var, HasCoeff] : Coeffs){
+      //llvm::outs() << Var->getNameAsString() << ": ";
+      if(HasCoeff){
+        //llvm::outs() << Var->getNameAsString() << " has coeff\n";
+        Order++;
+      }
+    }
+
+    return Order;
+  }
 
 
   void getCoefficients(clang::Stmt* S, std::map<clang::VarDecl*, std::string>* Coeffs){
@@ -165,7 +279,8 @@ private:
   }
 
 
-  int getOrder(clang::Expr* E){
+  int getOrder(clang::Expr* E, bool Output){
+    //llvm::outs() << "Getting order of " << Visitor_->Rewriter_.getRewrittenText(E->getSourceRange()) << "\n";
     // Constants and scalar references
     if(clang::isa<clang::DeclRefExpr>(E) || clang::isa<clang::IntegerLiteral>(E)){
       return 0;
@@ -179,9 +294,17 @@ private:
       if(clang::isa<clang::ImplicitCastExpr>(ASE->getIdx()) || clang::isa<clang::DeclRefExpr>(ASE->getIdx()))
         return 1;
       
-      //Linearized indexation expressions
+      // Linearized indexation expressions
       if(clang::BinaryOperator* IndexExpr = clang::dyn_cast<clang::BinaryOperator>(ASE->getIdx()))
         return delinearize(IndexExpr);
+    }
+    else if(clang::UnaryOperator* UO = clang::dyn_cast<clang::UnaryOperator>(E)){
+      // Array manipulation through pointers 
+      if(UO->getOpcode() == clang::UnaryOperator::Opcode::UO_Deref){
+        //llvm::outs() << "It's a pointer\n";
+        return recoverArray(UO, Output);
+      }
+
     }
     return 200;
   }
@@ -244,6 +367,7 @@ private:
 
   }
 
+
   bool mayBeMM(clang::BinaryOperator* Definition){
     // We heuristically assume that when the output array is indexed with a linear expression
     // that corresponds to a matrix multiplication kernel, so we do not add the  order of the 
@@ -252,6 +376,12 @@ private:
       clang::Expr* RHS = ASE->getIdx();
       if(clang::isa<clang::BinaryOperator>(*(RHS->child_begin()))){
         return multiplicationInDefinition(Definition->getRHS());
+      }
+    }
+    else if(clang::UnaryOperator* UO = clang::dyn_cast<clang::UnaryOperator>(Definition->getLHS())){
+      if(UO->getOpcode() == clang::UnaryOperator::Opcode::UO_Deref){
+        // Array manipulation through pointers 
+        return multiplicationInDefinition(UO);
       }
     }
     return false;
@@ -270,6 +400,14 @@ private:
     if(clang::ArraySubscriptExpr* ASE = clang::dyn_cast<clang::ArraySubscriptExpr>(S)){
       VarsAndCons->push_back(ASE);
       return;
+    }
+
+    if(clang::UnaryOperator* UO = clang::dyn_cast<clang::UnaryOperator>(S)){
+      // Array manipulation through pointers 
+      if(UO->getOpcode() == clang::UnaryOperator::Opcode::UO_Deref){
+        VarsAndCons->push_back(UO);
+        return;
+      }
     }
 
     for(auto& Child : S->children())
@@ -315,29 +453,37 @@ public:
       // We found an assignment to the output variable
       if(isParameter(Var) || isReturn(Var) || mayBeOutputAlias(Var, Def)){
         //llvm::outs() << "We need to analyze this\n";
-        Orders.push_back(getOrder(Def->getLHS()));
+        Orders.push_back(getOrder(Def->getLHS(), true));
         std::vector<clang::Expr*> VarsAndCons;
         getVariablesAndConstantsInDefinition(Def->getRHS(), &VarsAndCons);
         for(auto& Exp : VarsAndCons){
+          //llvm::outs() << "Vars and cons in defintion: \n";
           //llvm::outs() << Visitor_->Rewriter_.getRewrittenText(Exp->getSourceRange()) << "\n";
           //Exp->dump();
           // Constants
           if(clang::isa<clang::IntegerLiteral>(Exp)){
             Orders.push_back(0);
           }
+          else if(clang::UnaryOperator* UO = clang::dyn_cast<clang::UnaryOperator>(Exp)){
+            if(UO->getOpcode() == clang::UnaryOperator::Opcode::UO_Deref){
+              // Array manipulation through pointers 
+              Orders.push_back(getOrder(Exp, false));
+            }
+          }
           else{
             clang::VarDecl* V = getVariableDeclaration(getVariableReference(Exp));
             // References to parameters
             if(isParameter(V)){
-              Orders.push_back(getOrder(Exp));
+              Orders.push_back(getOrder(Exp, false));
             }
             // Local Variables need to have their own definition analysed
             else if(V->isLocalVarDecl()){
               std::vector<clang::Expr*> VarsAndConsInLocalVar;
               getParemetersAndConstantsInLocalVarDefinition(V, &VarsAndConsInLocalVar);
               for(auto& LocalVarExp : VarsAndConsInLocalVar){
+                //llvm::outs() << "Local vars in defintion: ";
                 //llvm::outs() << Visitor_->Rewriter_.getRewrittenText(LocalVarExp->getSourceRange()) << "\n";
-                Orders.push_back(getOrder(LocalVarExp));
+                Orders.push_back(getOrder(LocalVarExp, false));
               }
             }
           }
